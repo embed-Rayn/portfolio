694b88ae00c5acb0041e4bb97ee9fe208691ee48
embed-Rayn <57181674+embed-Rayn@users.noreply.github.com>
2020-10-19 17:33:46 +0900

    last touch, fix error

--- main.js
+++ main.js
@@ -1,98 +1,94 @@
-"use strict";
-// --------Navbar--------
+'use strict';
+
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector("#navbar");
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
+    navbar.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove("navbar--dark");
+    navbar.classList.remove('navbar--dark');
   }
 });
 
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
-}
-
-// Handle click on "contact me" button on home
-const contactMe = document.querySelector(".home__contact");
-contactMe.addEventListener("click", () => {
-  scrollIntoView("#contact");
-});
-
-// --------Home--------
-// Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector(".home__container");
-const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
-});
-
-//--------button--------
-// Show "arrow up" button when scrolling down
-const arrowUp = document.querySelector(".arrow-up");
-document.addEventListener("scroll", () => {
-  if (window.scrollY > homeHeight / 2) {
-    arrowUp.classList.add("visible");
-  } else {
-    arrowUp.classList.remove("visible");
-  }
-});
-
-//Handle click on the "arrow up" button
-arrowUp.addEventListener("click", () => {
-  scrollIntoView("#home");
-});
-
-//Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (event) => {
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  navbarMenu.classList.remove("open");
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
 
 // Navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
-navbarToggleBtn.addEventListener("click", () => {
-  navbarMenu.classList.toggle("open");
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
-//--------Work--------
-//Project
-/////////////////////////////////약간의 에러있음
-const workBtnContainer = document.querySelector(".work__categories");
-const projectContainer = document.querySelector(".work__projects");
-const projects = document.querySelectorAll(".project");
-workBtnContainer.addEventListener("click", (e) => {
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter == null) {
     return;
   }
-  // Remove selection from the previous item and select the new one.
-  const active = document.querySelector(".category_btn.selected");
-  active.classList.remove("selected");
-  const target =
-    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-  e.target.classList.add("selected");
 
-  projectContainer.classList.add("anim-out");
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  if (active != null) {
+    active.classList.remove('selected');
+  }
+  e.target.classList.add('selected');
 
+  projectContainer.classList.add('anim-out');
   setTimeout(() => {
-    projectContainer.classList.remove("anim-out");
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
   }, 300);
-  projects.forEach((project) => {
-    if (filter === project.dataset.type || filter === "*") {
-      project.classList.remove("invisible");
-    } else {
-      project.classList.add("invisible");
-    }
-  });
 });
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
